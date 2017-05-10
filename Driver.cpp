c22487eaf3447dfde1c580e9f0218b4acb4cb796
tedster827 <tedster827@gmail.com>
2017-05-09 20:51:54 -0700

    Adding Spacing/Testing Git

--- Driver.cpp
+++ Driver.cpp
@@ -1,10 +1,10 @@
 
 /**
  * Purpose: This is the main driver file which handles the order of task operations.
- * 
+ *
  * Authors Tobin deKorne', Teddy Williams, Rajak Jamel, Shrey Patel
  * May 9, 2017
- 
+
 public class Driver
 {
     public static void main(String[] args){
@@ -17,12 +17,11 @@ public class Driver
  */
 
 // Including the CPU header file that implements a CPU class
-#include "CPU.h"    
+#include "CPU.h"
 
 // Using the standard namespace of input/output
 using namespace std;
 
 int main() {
-
-return 0;     
+  return 0;     
 }
