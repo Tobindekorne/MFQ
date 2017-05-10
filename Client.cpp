fa9d100632b1d8eadf5bdd2bd2b11e8d8cc5759f
tedster827 <tedster827@gmail.com>
2017-05-09 20:55:11 -0700

    Changing Driver.cpp -> Client.cpp (More Readable)

---
+++ Client.cpp
@@ -0,0 +1,27 @@
+
+/**
+ * Purpose: This is the main driver file which handles the order of task operations.
+ *
+ * Authors Tobin deKorne', Teddy Williams, Rajak Jamel, Shrey Patel
+ * May 9, 2017
+
+public class Driver
+{
+    public static void main(String[] args){
+        MFQ mfq = new MFQ();
+        mfq.readInput();
+        while (!mfq.isFinished())
+            mfq.processInput();
+    }
+}
+ */
+
+// Including the CPU header file that implements a CPU class
+#include "CPU.h"
+
+// Using the standard namespace of input/output
+using namespace std;
+
+int main() {
+  return 0;     
+}
