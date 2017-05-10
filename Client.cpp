89a8d50bee197c3e9406a8241e0236f2e0396a8f
tedster827 <tedster827@gmail.com>
2017-05-09 21:29:34 -0700

    Verison 1.0.1

--- Client.cpp
+++ Client.cpp
@@ -1,19 +1,7 @@
-
-/**
+/*
  * Purpose: This is the client file which handles the order of task operations.
- *
  * Authors Tobin deKorne', Teddy Williams, Rajak Jamel, Shrey Patel
  * May 9, 2017
-
-public class Driver
-{
-    public static void main(String[] args){
-        MFQ mfq = new MFQ();
-        mfq.readInput();
-        while (!mfq.isFinished())
-            mfq.processInput();
-    }
-}
  */
 
 // Including the CPU header file that implements a CPU class
