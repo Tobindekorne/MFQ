b099fc98a10ab8cb168ed01bcd3fdde3927d2f2d
tedster827 <tedster827@gmail.com>
2017-05-13 18:06:42 -0700

    Adding the file stream library to base header file

--- Job.h
+++ Job.h
@@ -1,9 +1,10 @@
 #ifndef JOB_H
 #define JOB_H
 
+#include <fstream>
 class Job
 {
-    private: 
+    private:
       int arrTime;
       int pid;
       int cpuTime;
@@ -14,89 +15,89 @@ class Job
       int leaveQueue;
       int waitTime;
       int response;
-      
+
     public:
       Job(String s);
 
     /**
      * Decrements the job clock
-     * 
+     *
      */
       void decClock();
-    
+
     /**
      * Returns the arrival time of the job to the system.
-     * 
-     * @return     the arrival time of the job to the system. 
+     *
+     * @return     the arrival time of the job to the system.
      */
       int getArrTime();
-    
+
     /**
      * Returns the process identifier of the job.
-     * 
-     * @return     the process identifier. 
+     *
+     * @return     the process identifier.
      */
       int getPid()
-    
+
     /**
      * Checks whether or not the job has completed.
-     * 
+     *
      * @return     true if the job has completed. Otherwise, false.
      */
     bool isFinished();
-    
+
     /**
      * Increments the job to a lower level queue.
-     * 
+     *
      */
     void incLLQ();
-    
+
     /**
      * Returns the lowest level queue that the job entered.
-     * 
+     *
      * @return     the lowest level queue that the job entered.
      */
     int getLLQ();
-    
+
     /**
      * Returns the current value of the jobClock.
-     * 
-     * @return     the current value of the jobClock. 
+     *
+     * @return     the current value of the jobClock.
      */
     int getClock();
-    
+
     /**
      * Returns the time required for the cpu to process the job.
-     * 
-     * @return     the time required for the cpu to process the job. 
+     *
+     * @return     the time required for the cpu to process the job.
      */
     int getCpuTime();
-    
+
     /**
      * Sets the time that the job has entered into a queue.
-     *  
+     *
      */
     void enterQ(int time);
-    
+
     /**
      * Sets the time that the job leaves a queue.
-     * 
+     *
      */
     void leaveQ(int time);
 
     /**
      * Returns the time the job waited in queues through its lifetime.
-     * 
-     * @return     the time the job waited in queues through its lifetime. 
+     *
+     * @return     the time the job waited in queues through its lifetime.
      */
     int getWaitTime();
-    
+
     /**
      * Sets the time that it took for the job to start running after entering the system.
-     * 
-     * @param     the time the job started running. 
+     *
+     * @param     the time the job started running.
      */
     int getResponseTime(int time);
   };
-  
+
   #endif
